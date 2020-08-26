e4ef32020ebf76604bbeb83bf97cf6089d6c0c4b
Vu Van  Luan <luanvv@domain.vnext.vn>
2020-08-26 17:10:03 +0700

    Use my own jitsiLocalRecorder

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker-compose down
+sudo rm -rf ~/.jitsi-meet-cfg
+cp .env.example .env
+./gen-passwords.sh
+mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+docker-compose up -d
